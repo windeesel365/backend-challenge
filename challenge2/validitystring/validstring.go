31d86decf5fc077e856245eb210d892f21cf2c53
windeesel <windeesel365@gmail.com>
2024-06-11 22:01:58 +0700

    Challenge2: validitystring limitation

---
+++ challenge2/validitystring/validstring.go
@@ -0,0 +1,17 @@
+package validitystring
+
+import "log"
+
+func CheckString(s string) {
+	if len(s) > 5 {
+		log.Printf("Error: Please ensure to enter 5 characters")
+		return
+	}
+	for _, char := range s {
+		if char != 'L' && char != 'R' && char != '=' {
+			log.Printf("Error: String contains invalid character: %c", char)
+			return
+		}
+
+	}
+}
