947548fd01fc580a9bc2be8680075715ab8f754b
windeesel <windeesel365@gmail.com>
2024-06-12 00:14:47 +0700

    Challenge3: Update countwords

---
+++ challenge3/wordcount/countwords.go
@@ -0,0 +1,20 @@
+package wordcount
+
+import (
+	"regexp"
+	"strings"
+)
+
+// count occurrence  each word
+func CountMeats(text string) map[string]int {
+	re := regexp.MustCompile(`\b[a-zA-Z\-]+\b`)
+	words := re.FindAllString(text, -1)
+
+	counts := make(map[string]int)
+	for _, word := range words {
+		word = strings.ToLower(word)
+		counts[word]++
+	}
+
+	return counts
+}
