effbfc48de0e5e7b21c7568e93c24ab495cb36f6
windeesel <windeesel365@gmail.com>
2024-06-11 21:38:52 +0700

    Challenge1: maxpathsumcal

--- challenge1/main.go
+++ challenge1/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"maxpathsummod/jsonhandler"
-	//"summaxpath/jsonhandler"
-	//"summaxpath/maxpathsumcal"
+	"maxpathsummod/maxpathsumcal"
 )
 
 func main() {
@@ -12,7 +11,7 @@ func main() {
 	filename := "./jsonhandler/hard.json"
 	data := jsonhandler.ReadJSONFile(filename)
 	//เรียก func maxPathSum ในpackage maxpathsumcal
-	//result := maxpathsumcal.MaxPathSum(data)
+	result := maxpathsumcal.MaxPathSum(data)
 
 	fmt.Println(result)
 }
