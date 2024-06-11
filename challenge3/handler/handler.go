bea7eb6bba3a15b217b2986374d3884142f97161
windeesel <windeesel365@gmail.com>
2024-06-12 00:12:27 +0700

    Challenge3: Update handler

---
+++ challenge3/handler/handler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"countmeat/fetch"
+	"countmeat/wordcount"
+
+	"github.com/labstack/echo/v4"
+)
+
+// beefSummary รองรับ handles endpoint /beef/summary
+func BeefSummary(c echo.Context) error {
+	text, err := fetch.FetchMeatText()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	counts := wordcount.CountMeats(text)
+	return c.JSON(http.StatusOK, map[string]interface{}{"beef": counts})
+}
